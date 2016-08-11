4122c147ac855f0a40fab67b0985b8431fcce863
Ronen Lahat <ronenmail@gmail.com>
2016-08-11 13:18:03 +0300

    deleted old code

--- app/src/main/java/com/studio08/ronen/Zivug/Contact.java
+++ app/src/main/java/com/studio08/ronen/Zivug/Contact.java
@@ -18,10 +18,6 @@ public class Contact {
     private Date mDate;
     private String mName, mNotes, mPhone, mEmail, mPicturePath;
     private int mAge, mGender;
-//    private int mResourceId = 0;
-//    private int fillerResourceId;
-
-//    public boolean isImageSet = false; // maybe set a condition on the adapter to show the filler image, but right now there are many and associated with each contact
 
     private static Random r;
 
@@ -43,18 +39,6 @@ public class Contact {
         return mGender;
     }
 
-//    // avatars
-//    public static final int[] MALE_AVATARS = {R.drawable.avatar_01, R.drawable.avatar_02,
-//            R.drawable.avatar_03, R.drawable.avatar_04,
-//            R.drawable.avatar_11, R.drawable.avatar_12,
-//            R.drawable.avatar_13, R.drawable.avatar_14};
-//    public static final int[] FEMALE_AVATARS = {R.drawable.avatar_16, R.drawable.avatar_17,
-//            R.drawable.avatar_18, R.drawable.avatar_19,
-//            R.drawable.avatar_21, R.drawable.avatar_22,
-//            R.drawable.avatar_24};
-
-
-
     public UUID getId() {
         return mId;
     }
@@ -72,44 +56,6 @@ public class Contact {
         this.mAge = age;
     }
 
-//    public int getFillerResourceId() {
-//        return fillerResourceId;
-//    }
-//
-//    private void setFillerResourceId(int gender) {
-//        if (r == null) r = new Random();
-//
-//        if (gender == MALE) {
-//            int randomIndex = r.nextInt(MALE_AVATARS.length);
-//            fillerResourceId = MALE_AVATARS[randomIndex];
-//        } else if (gender == FEMALE) {
-//            int randomIndex = r.nextInt(FEMALE_AVATARS.length);
-//            fillerResourceId = FEMALE_AVATARS[randomIndex];
-//        } else {
-//            fillerResourceId = -1;
-//        }
-//    }
-
-//    public static int getInitialFillerResourceId(int gender) {
-//        if (r == null) r = new Random();
-//
-//        if (gender == MALE) {
-//            int randomIndex = r.nextInt(MALE_AVATARS.length);
-//            return MALE_AVATARS[randomIndex];
-//        } else if (gender == FEMALE) {
-//            int randomIndex = r.nextInt(FEMALE_AVATARS.length);
-//            return FEMALE_AVATARS[randomIndex];
-//        }
-//        return -1;
-//    }
-
-//    public int getResourceId() {
-//        if (mResourceId == 0)
-//            return getFillerResourceId();
-//        else
-//            return mResourceId;
-//    }
-
     public String getNotes() {
         return mNotes;
     }
@@ -124,12 +70,7 @@ public class Contact {
 
     public void setGender(int gender) {
         mGender = gender;
-//        setFillerResourceId(mGender);
     }
-
-//    public void setResourceId(int resourceId) {
-//        mResourceId = resourceId;
-//    }
 
     public void setPhone(String phone) {
         mPhone = phone;
